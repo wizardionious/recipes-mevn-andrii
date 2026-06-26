070f33c14a6cc04e0934e9f6fb7a33297a6cf4ad
Andrii <wizardionious.education@gmail.com>
2026-06-26 18:18:00 +0300

    feat(ui/button): add UIButton component

---
+++ src/shared/ui/button/UIButton.vue
@@ -0,0 +1,38 @@
+<script lang="ts" setup>
+interface Props {
+  variant?: "primary" | "secondary" | "ghost" | "danger";
+  size?: "sm" | "md" | "lg";
+  type?: "button" | "submit" | "reset";
+  disabled?: boolean;
+  loading?: boolean;
+}
+
+const {
+  variant = "primary",
+  size = "md",
+  type = "button",
+  disabled = false,
+  loading = false,
+} = defineProps<Props>();
+</script>
+
+<template>
+  <button
+    class="base-button"
+    :class="[
+      `ui-button--${variant}`,
+      `ui-button--${size}`,
+      loading && 'ui-button--loading',
+      disabled && 'ui-button--disabled',
+    ]"
+    :type="type"
+    :disabled="disabled || loading"
+  >
+    <span v-if="loading" class="ui-button__loader" />
+    <slot />
+  </button>
+</template>
+
+<style lang="scss" scoped>
+@use "./button";
+</style>
